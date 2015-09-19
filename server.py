e3d3ccc2c057598ccd0086de04a0d3e710b7d163
dchoi2 <dchoi2@mit.edu>
2015-09-19 19:20:18 -0400

    added content reception

--- server.py
+++ server.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
+def process_content(content):
+  return len(content)
+
 @app.route("/")
-def hello():
-  return "Hello World!"
+def home():
+  return "Hello World!" 
+
+@app.route("/content/")
+def process(): 
+  songs = process_content(request.args.get('content'))
+  return str(songs)
 
 if __name__ == "__main__":
   app.run()
