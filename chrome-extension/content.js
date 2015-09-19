444e4ca84065f25b252c03ee88ae2cc36c6fbe6f
Akshay <akshayr@mit.edu>
2015-09-19 16:58:41 -0400

    added sc, doesnt work yet

---
+++ chrome-extension/content.js
@@ -0,0 +1,13 @@
+console.log("hi");
+t = $('body').text();
+console.log(t);
+$('body').append(t);
+
+SC.initialize({
+  client_id: 'bdc283e42a80983fd9bea0fa8871be8a'
+});
+
+// stream track id 293
+SC.stream("/tracks/293", function(sound){
+  sound.play();
+});
