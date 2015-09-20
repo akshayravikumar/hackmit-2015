8c3f956430341e8b72146c7a7e46ba9375259004
dchoi2 <dchoi2@mit.edu>
2015-09-20 01:35:41 -0400

    content processing

--- server.py
+++ server.py
@@ -1,16 +1,21 @@
 from flask import Flask, request
+from processing.identify_keywords import identify_keywords
 app = Flask(__name__)
 
 def process_content(content):
-  return len(content)
+  x = identify_keywords(content)
+  return x.keys()
 
 @app.route("/")
 def home():
   return "Hello World!" 
 
 @app.route("/content/")
-def process(): 
-  songs = process_content(request.args.get('content'))
+def process():
+  if (request.args.get('content')):
+    songs = process_content(request.args.get('content'))
+  else:
+    songs = "Hello World!"
   return str(songs)
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ setup(
   description      = 'OpenShift App',
   author           = '',
   author_email     = '',
-  url              = 'http://www.python.org/sigs/distutils-sig/',i
+  url              = 'http://www.python.org/sigs/distutils-sig/',
   packages         = [
   ],
   install_requires = [
     'flask == 0.10.1',
+    'requests == 2.7.0'
   ]
 )
 
