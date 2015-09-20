7106cef81eb4923bdf3e8e55c6535f9c58745c72
dchoi2 <dchoi2@mit.edu>
2015-09-20 03:29:50 -0400

    working server code

--- play_hackmit/server.py
+++ play_hackmit/server.py
@@ -6,17 +6,23 @@ def process_content(content):
   x = identify_keywords(content)
   return x.keys()
 
+def process_images(images):
+  return images
+
 @app.route("/")
 def home():
   return "Hello World!" 
 
-@app.route("/content/")
+@app.route("/content/", methods = ["POST"])
 def process():
-  if (request.args.get('content')):
-    songs = process_content(request.args.get('content'))
-  else:
-    songs = "Hello World!"
+  print request.data
+  songs = "Hi"
+  if (request.form['content']):
+    songs_txt = process_content(request.form['content'])
+  if (request.form['images']):
+    songs_img = process_images(request.form['images'])
+  songs = {"content": songs_txt, "images":songs_img}
   return str(songs)
 
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
